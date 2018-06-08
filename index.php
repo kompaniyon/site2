e7eb6195a071fb785e8eded49db9074992203ac8
kompaniyon <kapitanchuk@gmail.com>
2018-06-08 09:57:28 +0300

    initial project version

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TN</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css"> 
+	<link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet"> 
+</head>
+<body>
+ <header id="header" class="header">
+ <div class="container">
+	    <div class="row">
+	      <div class="col-lg-1">
+	       TN
+	      </div>
+	    
+	 
+	    <div class="col-lg-7 offset-lg-4">
+		<ul class="vmenu">
+	      <li><a href="#">Новини</a></li>
+	      <li><a href="">Каталог</a></li>
+	      <li><a href="">Сервіс</a></li>
+	      <li><a href="">Контакти</a></li>
+	      <li><a href="">Про нас</a></li>	
+       </ul>	  
+	  </div></div>
+	  </div>
+ </header>
+	<section id="con" class="con">
+	<div class="container">
+	
+	</div>
+	</section> 
+	<footer>
+	
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
